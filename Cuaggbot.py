2dfcf85e17863054c68c8d1de7e813a2d6e7a422
Cuagga <66082268+Cuagga@users.noreply.github.com>
2021-07-02 14:59:06 +0200

    Add files via upload

--- Cuaggbot.py
+++ Cuaggbot.py
@@ -1,50 +1,70 @@
-import requests, time
+import requests
+import time
 
+soft_headers = {"User-Agent" : "CuaggBot (https://fr.wikipedia.org/wiki/Utilisateur:Cuaggbot)"}
+botUN ='Cuaggbot'
+botPW = 'X9LnvynUASfFDCR'
 
-#Connexion au compte du bot
+url = 'https://fr.wikipedia.org/w/api.php'
+
 S = requests.Session()
 
+def login():
+    #get login token
+    params_0 = {
+    		"action":"query",
+    		"meta":"tokens",
+    		"type":"login",
+    		"format":"json"
+    		}
+    current_request = S.get(url = url, params = params_0, headers = soft_headers)
+    current_data = current_request.json()
+    login_token = current_data["query"]["tokens"]["logintoken"]
+    
+    #try to login
+    params_1 = {
+    "action":"login",
+    "lgname": botUN,
+    "lgpassword": botPW,
+    "lgtoken": login_token,
+    "format":"json"
+    		}
+    		
+    current_request = S.post(url, data = params_1, headers=soft_headers)
+    current_data = current_request.json()
+    login_result = current_data["login"]["result"]
+    if login_result == "Success":
+        return True
+    else:
+        return False
 
-URL = "https://fr.wikipedia.org/w/api.php"
-
-# Login token 
-PARAMS_0 = {
-    'action':"query",
-    'meta':"tokens",
-    'type':"login|csrf",
-    'format':"json"
-}
-
-R = S.get(url=URL, params=PARAMS_0)
-DATA = R.json()
-loginToken = DATA['query']['tokens']['logintoken']
-
-
-
-PARAMS_1 = {
-    'action':"login",
-    'lgname':"Cuaggbot",
-    'lgpassword':"X9LnvynUASfFDCR",
-    'lgtoken':loginToken,
-    'format':"json"
-}
-
-R = S.post(URL, data=PARAMS_1)
-
-PARAMS_2 = {
-    "action": "query",
-    "meta": "tokens",
-    "format": "json"
-}
-
-R = S.get(url=URL, params=PARAMS_2)
-DATA = R.json()
-
-editToken = DATA['query']['tokens']['csrftoken']
-
+def getEditToken(page):
+    params_0 = {
+		"action":"query",
+		"meta":"tokens|userinfo",
+		"type":"csrf",
+		"format":"json",
+		"curtimestamp":"1",
+		"prop":"revisions",
+		"rvprop":"timestamp",
+		"rvslots":"*",
+		"uiprop":"hasmsg",
+		"titles": page,
+		"indexpageids":"1"
+		}
+    current_request = S.get(url=url, params= params_0, headers=soft_headers)
+    current_data = current_request.json()
+    edit_token = current_data["query"]["tokens"]["csrftoken"]
+    page_id = current_data["query"]["pageids"][0] #retrieve the page_title id, needed for the next line.
+    try:
+        last_revision_time = current_data["query"]["pages"][page_id]["revisions"][0]["timestamp"]
+    except :
+        last_revision_time = None
+    current_time = current_data["curtimestamp"]
+    return edit_token, last_revision_time, current_time
 
 def inport(page):
-    """Permet d'importer une page donnée en paramètre"""
+
     URL = "https://fr.wikipedia.org/w/api.php"
     
     PARAMS = {
@@ -54,10 +74,12 @@ def inport(page):
         "format": "json"
     }
     
-    R = S.get(url=URL, params=PARAMS)
+    R = requests.get(url=URL, params=PARAMS)
     DATA = R.json()
-    
-    return DATA["parse"]["wikitext"]["*"]
+    try:
+        return DATA["parse"]["wikitext"]["*"]
+    except:
+        return -1
     
 def fetch(page, chaine):
     try:
@@ -77,7 +99,7 @@ def browse(cat, cmcont=''):
         'cmlimit':'500',
         'cmcontinue':cmcont
         }
-    r = S.post(url=URL, params=PARAMS)
+    r = requests.post(url=URL, params=PARAMS)
     data = r.json()
     for k in data['query']['categorymembers']:
         scope.append(k['title'])
@@ -91,63 +113,83 @@ def browse(cat, cmcont=''):
             suite = False
         except:
             suite = False
+    print(scope)
     return scope
 
+def edit(page, text, summary = '', minor=1, isBot=1):
+    wait = 5
+    retry = True
+    while retry == True:
+        tupel = getEditToken(page)
+        try:
+            editToken, lastRevTime, curTimeStamp = tupel
+        except:
+            continue
+        params0 = {
+			"action":"edit",
+			"format":"json",
+			"title": page,
+			"text": text,
+			"summary": summary,
+			"minor": minor,
+			"bot": isBot,
+			"token": editToken,
+			"basetimestamp": lastRevTime,
+			"starttimestamp": curTimeStamp,
+			"assert":"user",
+			"maxlag": 5
+			}
+        req = S.post(url, data=params0)
+        data = req.json()
+        try:
+            result = data['edit']['result']
+            if result=='Success':
+                retry = False
+                print('Succès')
+            else:
+                print('Echec')
+                time.sleep(wait)
+        except:
+            print('Erreur')
+            time.sleep(wait)
 
 # Bout de code Evaluation
 def éval(projet):
     scope = []
     #Remplissage du scope
-    portail = browse('Catégorie:Portail:'+projet+'/Articles liés')
+    scope = browse('Catégorie:Portail:'+projet+'/Articles liés')
     for k in scope: #Traitement des articles présents dans le scope 
-        time.sleep(20)
         page = inport('Discussion:' + k)
+        if page == -1:
+            page = ''
         test = fetch(page, '{{Wikiprojet')
+        if test == -1:
+            test = fetch(page, '{{wikiprojet')
         if test==-1:
             prep = "{{Wikiprojet|"+projet+"|importance=?|avancement=?}}"
-            PARAMS = {
-                'action':'edit',
-                'title':'Discussion:'+k,
-                'section':'0',
-                'prepend':prep,
-                'format':'json',
-                'token':editToken
-                }
-            r = S.post(url = 'https://fr.wikipedia.org/w/api.php', params = PARAMS)
+            page = prep + page
+            edit('Discussion:'+k, page, summary='Apposition du modèle Wikiprojet|Abbeville')
+            time.sleep(15)
         else:
             pageA = page[:test+13]
             pageB = page[test:]
+            
             pageB = pageB[:fetch(pageB, 'avancement')]
-            pageB.split('|') #Séparation des paramètres du modèle
+            pageB = pageB.split('|') #Séparation des paramètres du modèle
             projets = []
-            for j in range(len(pageB//2)):
+            for j in range(len(pageB)//2):
                 projets.append(pageB[2*j+1]) #Récupération des projets évalués
             if projet not in projets:
                 pageC = page[test+13:]
-                supp = projet + '|?'
+                supp = '|' + projet + '|?'
                 page = pageA + supp + pageC
-                PARAMS = {
-                    'action':'edit',
-                    'title':'Discussion:'+k,
-                    'bot':'true',
-                    'token':editToken,
-                    'text':page,
-                    'summary':'Apposition du modèle Wikiprojet pour le projet '+ projet
-                    }
-                r = S.post(url='https://fr.wikipedia.fr/w/api.php', params=PARAMS)
-
-#Requête de test
+                edit('Discussion:'+k, page, summary='Apposition du modèle Wikiprojet|Abbeville')
+                time.sleep(15)
+            else:
+                pass
 
 
 
-
-
-
-
-
-
-
-
-
+login()
 
 
