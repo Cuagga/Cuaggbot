92c00cc9b14f8e0c27be6d4924dbb2641baf8441
Cuagga <66082268+Cuagga@users.noreply.github.com>
2021-10-04 21:59:32 +0200

    Add files via upload

--- Cuaggbot.py
+++ Cuaggbot.py
@@ -3,7 +3,7 @@ from cuaggMods import *
 
 soft_headers = {"User-Agent" : "CuaggBot (https://fr.wikipedia.org/wiki/Utilisateur:Cuaggbot)"}
 botUN ='Cuaggbot'
-botPW = 'X9LnvynUASfFDCR'
+botPW = 'XXXXXXXXX'
 
 url = 'https://fr.wikipedia.org/w/api.php'
 
